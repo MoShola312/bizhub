98a042c097f6c1d9b695797e36e481ca4efc6bba
Mo Sho <mohammed.sholaja@gmail.com>
2020-09-10 19:32:41 -0500

    create if statemate in ajax to redirect that page if the user isn't logged in

--- public/ajax.js
+++ public/ajax.js
@@ -1,35 +1,43 @@
+
 //like button
 $('.likeForm').on('submit', function(e){
     e.preventDefault();
     let actionUrl = $(this).attr('action')
-    console.log(`url: ${actionUrl}`)
+	console.log(`url: ${actionUrl}`)
+	let likeBtn = $(this).find('.likedBtn1')
 
 $.ajax({ 
-    url: actionUrl, 
+	url: actionUrl,
+	btn: likeBtn, 
     data:'',
     type: "POST",
     
     success: function(response) {
 		
-		console.log(response)
+		//if user isn't logged in redirects them to login screen
+		if(response.foundUserLike == undefined){
+			
+			window.location.href ="/login";
+			
+		}
 		// console.log(`review.like: ${response.review.likes.length}`)
 		// console.log(`founduser: ${response.foundUserLike}`)
 	   
 		//if foundUserLike is "true", unlike the button, pull LIKE
-
-		
-		if (response.foundUserLike) { 
+		else if (response.foundUserLike) { 
 			console.log(`founduser:  ${response.foundUserLike}`)
-			$("#" + response.review._id).attr('class', 'btn btn-sm btn-secondary likedBtn1').html(`<i class="fas fa-thumbs-up"></i> Like (${response.review.likes.length})`)
-			$("#" + response.review._id).blur()
+			// $("#" + response.review._id).attr('class', 'btn btn-sm btn-secondary likedBtn1').html(`<i class="fas fa-thumbs-up"></i> Like (${response.review.likes.length})`)
+			$(likeBtn).attr('class', 'btn btn-sm btn-secondary likedBtn1').html(`<i class="fas fa-thumbs-up"></i> Like (${response.review.likes.length})`)
+			$(likeBtn).blur()
+			// debugger
 		} else {
 			console.log(`else founduser:  ${response.foundUserLike}`)
-			//foundUserLike is "false" means user didn't LIKE it yet, like the button, add LIKE
+			//foundUserLike is "false" means user didn't LIKE it yet, add LIKE
 			//change the class of the button
 			//change the icon text of the font awesome
-			$("#" + response.review._id).attr('class', 'btn btn-sm btn-primary likedBtn1').html(`<i class="fas fa-thumbs-up"></i> Liked (${response.review.likes.length})`)
-			}
-			
+			// $("#" + response.review._id).attr('class', 'btn btn-sm btn-primary likedBtn1').html(`<i class="fas fa-thumbs-up"></i> Liked (${response.review.likes.length})`)
+			$(likeBtn).attr('class', 'btn btn-sm btn-primary likedBtn1').html(`<i class="fas fa-thumbs-up"></i> Liked (${response.review.likes.length})`)
+			}	
 	}
 })
 })
